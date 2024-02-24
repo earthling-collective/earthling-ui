553ecf54a5d4106dc7cec450b29267230b863365
Steven Frady <sfrady20@gmail.com>
2024-02-23 23:10:17 -0500

    upgrade

--- sandboxes/vite/src/App.tsx
+++ sandboxes/vite/src/App.tsx
@@ -1,68 +1,10 @@
 import { Box, Text } from "@earthling-ui/primitives";
-import { useEffect, useState } from "react";
-
-atomic("w-full h-[300px] inset-0");
-
-const BizzBox = styled(Box)
-  .using(defaultColorTokens)
-  .extend({
-    width: "100px",
-    height: "100px",
-    backgroundColor: "green",
-    color: "#77FF77",
-
-    "[foo]": {
-      backgroundColor: "$blue.500",
-      color: "#000000",
-
-      "[bizz]": {
-        color: "$blue.800",
-      },
-
-      "[buzz]": {
-        color: "$blue.200",
-      },
-    },
-
-    "[bar]": {
-      backgroundColor: "$red.500",
-      color: "#000000",
-
-      "[bizz]": {
-        color: "$red.200",
-      },
-
-      "[buzz]": {
-        color: "$red.800",
-      },
-    },
-  })
-  .build();
-
-const useFlippingCondition = (timeout: number, on: string, off: string) => {
-  const [condition, setCondition] = useState(on);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCondition((x) => (x === on ? off : on));
-    }, timeout);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [timeout, on, off]);
-  return condition;
-};
 
 function App() {
-  const condition1 = useFlippingCondition(3000, "foo", "bar");
-  const condition2 = useFlippingCondition(5200, "bizz", "buzz");
-
   return (
-    <BizzBox
-      extended={{ ...atomic("w-full"), backgroundColor: "$yellow.500" }}
-      with={[condition1, { [condition2]: true }]}
-    >
+    <Box>
       <Text>hello?</Text>
-    </BizzBox>
+    </Box>
   );
 }
 
