752d5cfb22dffeab8dab5bbbc8fe41326ba05ae6
Steven Frady <sfrady20@gmail.com>
2025-01-27 15:06:25 -0500

    build script, tsconfig test

---
+++ packages/earthling-ui/scripts/build.ts
@@ -0,0 +1,9 @@
+Bun.build({
+  entrypoints: [
+    "src/components/button/index.tsx",
+    "src/components/card/index.tsx",
+    "src/components/input/index.tsx",
+    "src/components/textarea/index.tsx",
+  ],
+  outdir: "dist",
+});
