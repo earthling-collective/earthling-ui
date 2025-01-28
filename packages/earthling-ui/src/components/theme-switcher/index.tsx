29fe7826f32de58aa95a4774930974785dcde88d
Steven Frady <sfrady20@gmail.com>
2025-01-27 23:38:22 -0500

    theme switcher fixes

--- packages/earthling-ui/src/components/theme-switcher/index.tsx
+++ packages/earthling-ui/src/components/theme-switcher/index.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/utils/cn";
+import { cn } from "../../utils/cn";
 import { cva, type VariantProps } from "class-variance-authority";
 import { forwardRef, type ComponentProps, type ComponentRef } from "react";
 
