353de90a432dc5b712a565a2eb0f3747d7d48df1
Steven Frady <sfrady20@gmail.com>
2025-05-02 01:52:32 -0400

    fixed docs code block styling

--- apps/docs/src/mdx-components.tsx
+++ apps/docs/src/mdx-components.tsx
@@ -1,12 +1,17 @@
 import type { MDXComponents } from "mdx/types";
 import { WorkInProgress } from "./components/wip";
 import { Code } from "./components/code";
+import { cn } from "earthling-ui/utils/cn";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
 
     WIP: WorkInProgress,
+
+    pre: ({ className, ...props }) => {
+      return <pre {...props} className={cn("not-prose", className)} />;
+    },
 
     code: (props) => {
       const lanugageClassName = /language-(\w+)/.exec(
